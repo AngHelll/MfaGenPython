e849f9dcdae59aaf8aa61f6acbd55356069ca6da
Angel JimenezR <hell_boy@live.com.mx>
2023-02-15 04:49:06 -0600

    add adjust inputs and read inputs

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sys
 
-from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
+
 from gui_app import Ui_MainWindow
 
 
@@ -9,6 +9,14 @@ class App_Mfa(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(App_Mfa, self).__init__(parent)
         self.setupUi(self)
+        self.btn_generar.clicked.connect(self.fn_generar_totp)
+
+    def fn_generar_totp(self):
+        self.fn_validar_campos()
+
+    def fn_validar_campos(self):
+        input_text_account = self.inp_account.text()
+        print(input_text_account)
 
 
 if __name__ == '__main__':
