5c08154df45f4127259c01f9265ad6d58cc9e6e3
Angel JimenezR <hell_boy@live.com.mx>
2023-02-16 02:33:45 -0600

    add funtionality and adjust visual

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import datetime
 import sys
-
-from PyQt5.QtWidgets import QMainWindow, QApplication
+import pyotp
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
 
 from gui_app import Ui_MainWindow
 
@@ -12,11 +13,25 @@ class App_Mfa(QMainWindow, Ui_MainWindow):
         self.btn_generar.clicked.connect(self.fn_generar_totp)
 
     def fn_generar_totp(self):
-        self.fn_validar_campos()
-
-    def fn_validar_campos(self):
-        input_text_account = self.inp_account.text()
-        print(input_text_account)
+        if self.inp_shared_key.text() != "" and self.inp_account.text() != "":
+            sharedKey = self.inp_shared_key.text()
+            if True:
+                totp = pyotp.TOTP(sharedKey)
+                codigo = str(totp.now())
+                self.lbl_totp.setText(codigo)
+                time_remaining = totp.interval - datetime.datetime.now().timestamp() % totp.interval
+                self.lbl_time.setText(str(int(time_remaining)))
+                position = self.tbw_hystoric.rowCount()
+                self.tbw_hystoric.insertRow(position)
+                self.tbw_hystoric.setItem(position, 0, QTableWidgetItem(str(datetime.datetime.now())))
+                self.tbw_hystoric.setItem(position, 1, QTableWidgetItem(str(sharedKey)))
+                self.tbw_hystoric.setItem(position, 2, QTableWidgetItem(str(codigo)))
+                self.tbw_hystoric.setItem(position, 3, QTableWidgetItem(str(self.inp_account.text())))
+            else:
+                self.lbl_totp.setText("No has ingresado los campos requeridos")
+        else:
+            self.lbl_totp.setText("No has ingresado los campos requeridos")
+    #
 
 
 if __name__ == '__main__':
